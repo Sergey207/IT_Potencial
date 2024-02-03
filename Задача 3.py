7da58c039bcc674ec7b01f0a6b15b2a48067aae8
Sergey <67117006+Sergey207@users.noreply.github.com>
2024-02-03 16:12:21 +0300

    Задача 3

---
+++ Задача 3.py
@@ -0,0 +1,32 @@
+import csv
+
+ships = []
+
+
+def getName(shipName: str) -> str:
+    """
+    This function returns formatted info about ship
+    :param shipName: str
+    :return: formatted string
+    """
+    for i in ships:
+        if i[0] == shipName:
+            return f"Корабль {i[0]} был отправлен с планеты: {i[1]} и его направление движения было: {i[2]}"
+    return "error.. er.. ror.."
+
+
+# Ввод данных
+with open('space.txt', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter='*')
+    for line in reader:
+        if line[0] != 'ShipName':
+            ships.append([line[0], line[1], line[3]])
+
+# Бесконечный ввод
+run = True
+while run:
+    ui = input()
+    if ui == 'stop':
+        run = False
+    else:
+        print(getName(ui))
