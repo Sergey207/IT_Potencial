006f733e2e351af7b52e812dd042f9ebbb4d4dd6
Sergey <67117006+Sergey207@users.noreply.github.com>
2024-02-03 16:29:31 +0300

    Задача 4

---
+++ Задача 4.py
@@ -0,0 +1,28 @@
+import csv
+
+res = []
+
+
+def generate_password(shipName: str, planet: str) -> str:
+    """
+    Returns password by data
+    :param shipName: str
+    :param planet: str
+    :return: password
+    """
+    return f'{planet[-3:]}{shipName[2:0:-1]}{planet[2::-1]}'.upper()
+
+
+# Ввод данных
+with open('space.txt', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter='*')
+    for line in reader:
+        if line[0] == 'ShipName':
+            res.append(line + ['password'])
+        else:
+            res.append(line + [generate_password(*line[:2])])
+
+# Вывод данных в файл space_uniq_password.csv
+with open('space_uniq_password.csv', mode='w', encoding='utf-8') as f:
+    for line in res:
+        print('*'.join(line), file=f)
